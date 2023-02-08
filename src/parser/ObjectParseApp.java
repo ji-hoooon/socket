068cd2e855fb01b3dab9f0604be1d130486bf808
ji-hoooon <ljh6384@naver.com>
2023-02-08 21:19:50 +0900

    ADD Json fromtoJson by Gson

--- src/parser/ObjectParseApp.java
+++ src/parser/ObjectParseApp.java
@@ -1,4 +1,19 @@
 package parser;
 
+import com.google.gson.Gson;
+
 public class ObjectParseApp {
+
+    public static void main(String[] args) {
+        //\n은 문자열을 감싸는 쌍따옴표가 아니라는 뜻
+        String json ="{\"id\":6,\"gubun\":\"입금\",\"sender\":\"ATM\",\"receiver\":\"1111\",\"amount\":100}\n";
+        //문자열로 들어온 데이터를 객체로 변환 :역직렬화
+        Gson gson = new Gson();
+        Transaction transaction =gson.fromJson(json, Transaction.class);
+        System.out.println("transaction.getId() = " + transaction.getId());
+        System.out.println("transaction.getAmount() = " + transaction.getAmount());
+        System.out.println("transaction.getGubun() = " + transaction.getGubun());
+        System.out.println("transaction.getReceiver() = " + transaction.getReceiver());
+        System.out.println("transaction.getSender() = " + transaction.getSender());
+    }
 }
